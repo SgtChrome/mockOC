234b3b84ee114b8ed82edaee2d70f92db6b32e5b
SgtChrome <fred.sausebrett@gmail.com>
2022-10-11 10:17:45 +0000

    switch to interlockingExperiment.py init interlockingExperiment.py

--- object-controller/interlockingExperiment.py
+++ object-controller/interlockingExperiment.py
@@ -1,6 +1,7 @@
 import socketserver
 import threading
 import socket
+import sys
 
 import os
 import logging
@@ -27,9 +28,15 @@ class RASTA_CODES:
 REQUEST_RECONNECT = '0'
 REQUEST_CONNECTIONLIST = '1'
 
+repetitions = 100
+expName = ''
+waittime = 1
+
 inst = None
+loopThread = threading.Thread()
+countReceived = 0
 countSent = 0
-WAITTIME = 4
+
 
 class RaSTA:
     def __init__(self, name, ID, ip) -> None:
@@ -62,24 +69,17 @@ def getTimestamp():
 
 
 def getOrderID():
-    global countSent
-    string = f"Exp{str(countSent)}+{shortuuid.uuid()[10:]}"
+    global countSent, expName
+    string = f"{expName}Exp{str(countSent)}+{shortuuid.uuid()[10:]}"
     countSent += 1
     return string
     #return shortuuid.uuid()
 
 class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
 
     def handle(self):
         data = self.request[0].strip()
-        socket = self.request[1]
-        #logging.debug(datetime.now().strftime("%H:%M:%S-%f") + ' - ' + self.client_address[0] + ':' + data.decode("utf-8"))
+        #socket = self.request[1]
         print(datetime.now().strftime("%H:%M:%S-%f") + ' - ' + self.client_address[0] + ': ' + data.decode("utf-8"))
         #sleep(3)
         data = data.decode("utf-8")
@@ -87,18 +87,41 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
 
         cur_thread = threading.current_thread()
 
+        global countReceived, countSent, repetitions, expName, waittime, loopThread
+
         if received[0] == '0':
-            print(received[3])
+            #print(received[3])
             orderID, message = received[3].split('-')
             if 'startup confirmed' in message:
-                loop_thread = threading.Thread(target=loopMessages)
-                loop_thread.daemon = True
-                loop_thread.start()
-            logging.info(f'Epoch:{getTimestamp()} - [Interlocking_RECEIVED] {data}')
+                print('startup confirmed')
+                return
 
+            logging.info(f'Epoch:{getTimestamp()} - [Interlocking_RECEIVED] {data}')
+            countReceived += 1
+
+        elif received[0] == '3':
+            print('Received experiment start', received)
+            countReceived = 0
+            countSent = 0
+            repetitions = int(received[1])
+            waittime = float(received[2])
+            expName = str(received[3])
+            if loopThread:
+                if loopThread.is_alive():
+                    loopThread.join()
+            loopThread = threading.Thread(target=loopMessages)
+            loopThread.daemon = True
+            loopThread.start()
+
+        elif received[0] == '2':
+            exit()
+
+        if countReceived == repetitions:
+            print('Finished experiment with', repetitions, 'repetitions')
 
         # socket.sendto(str.encode(message), (RASTAIP, RASTAPORT))
         # 0/1 Message/Internal; RastaID_Sender; RastaID_Receiver; orderId - message
+
 
 class ThreadedUDPServer(socketserver.ThreadingMixIn, MyUDPHandler):
     pass
@@ -107,7 +130,7 @@ def sendOrder(udpSocket, order):
     try:
         udpSocket.sendto(str.encode(order), (RASTAIP, RASTAPORT))
         logging.info(f'Epoch:{getTimestamp()} - [Interlocking_SENT] {order}')
-        print(f'Epoch:{getTimestamp()} - [Interlocking_SENT]:{order}')
+        #print(f'Epoch:{getTimestamp()} - [Interlocking_SENT]:{order}')
     except socket.gaierror as err:
         if err.errno == -2:
             print("Rasta socket is down")
@@ -115,13 +138,26 @@ def sendOrder(udpSocket, order):
             print(err)
 
 def loopMessages():
-    while(True):
+    print("Repetitions:", repetitions)
+    while(countSent < repetitions):
         sendOrder(UDPClientSocket, "0;%s;%s;%s-left" % (inst.clients['interlocking'].rastaID, inst.clients['switch1'].rastaID, getOrderID()))
-        sleep(WAITTIME)
+        sleep(waittime)
         sendOrder(UDPClientSocket, "0;%s;%s;%s-right" % (inst.clients['interlocking'].rastaID, inst.clients['switch1'].rastaID, getOrderID()))
-        sleep(WAITTIME)
+        sleep(waittime)
 
 if __name__ == "__main__":
+    """ if len(sys.argv) < 2:
+        pass
+    else:
+        #repetitions = int(sys.argv[1])
+        #print(f"Repetitions: {str(repetitions)}")
+        expName = sys.argv[1]
+        print(f"ExpName String: {expName}") """
+
+
+    """ expName = os.environ.get('EXP_NAME')
+    repetitions = int(os.environ.get('REPETITIONS')) if os.environ.get('REPETITIONS') else 10 """
+
     if not os.path.exists('internalConfig.yaml'):
         print("No config file found")
         exit()
@@ -138,6 +174,9 @@ if __name__ == "__main__":
             logging.StreamHandler()
         ], encoding='utf-8', level=logging.DEBUG)
 
+    #countLock = threading.Lock()
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
     receiver = socketserver.UDPServer((HOST, RECEIVERPORT), ThreadedUDPServer)
     server_thread = threading.Thread(target=receiver.serve_forever)
     server_thread.daemon = True
@@ -145,5 +184,4 @@ if __name__ == "__main__":
 
     print("Object controller up!")
 
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     server_thread.join()
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker compose up --build -d
+DOCKERFILE_OC=DockerfileOCInterlocking DOCKERFILE_RASTA=DockerfileRastaInterlocking EXP_NAME=Test docker compose up --build -d
 docker exec -it sender_rasta ./interlocking
